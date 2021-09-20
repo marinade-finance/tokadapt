fa295c894df85d2f60dc344596f490989a8d9562
aankor <lambdasound@yandex.ru>
2021-09-20 16:37:49 +0300

    first commit

---
+++ programs/tokadapt/src/lib.rs
@@ -0,0 +1,238 @@
+use anchor_lang::{prelude::*, solana_program::system_program};
+use anchor_spl::token::{self, Mint, Token, TokenAccount};
+
+declare_id!("CWZ8fFJLqLPGgxKF7zzJcs4hVnUfZAPbVdMGaGtMHfaa");
+
+#[program]
+pub mod tokadapt {
+    use super::*;
+
+    pub fn initialize(
+        ctx: Context<Initialize>,
+        admin_authority: Pubkey,
+        input_mint: Pubkey,
+    ) -> ProgramResult {
+        ctx.accounts.process(admin_authority, input_mint)
+    }
+
+    pub fn swap(ctx: Context<Swap>, amount: u64) -> ProgramResult {
+        ctx.accounts.process(amount)
+    }
+
+    pub fn close(ctx: Context<Close>) -> ProgramResult {
+        ctx.accounts.process()
+    }
+}
+
+#[account]
+pub struct State {
+    pub admin_authority: Pubkey,
+    pub input_mint: Pubkey,
+    pub output_storage: Pubkey,
+    pub output_storage_authority_bump: u8,
+}
+
+impl State {
+    pub const OUTPUT_STORAGE_AUTHORITY_SEED: &'static [u8] = b"storage";
+}
+
+#[derive(Accounts)]
+pub struct Initialize<'info> {
+    #[account(zero, rent_exempt = enforce)]
+    pub state: Account<'info, State>,
+    #[account()]
+    pub output_storage: Account<'info, TokenAccount>,
+}
+
+impl<'info> Initialize<'info> {
+    pub fn process(&mut self, admin_authority: Pubkey, input_mint: Pubkey) -> ProgramResult {
+        let (output_storage_authority, output_storage_authority_bump) =
+            Pubkey::find_program_address(
+                &[
+                    State::OUTPUT_STORAGE_AUTHORITY_SEED,
+                    &self.state.key().to_bytes(),
+                ],
+                &ID,
+            );
+        if self.output_storage.owner != output_storage_authority {
+            msg!(
+                "Expected output storage authority to be {}",
+                output_storage_authority
+            );
+            return Err(ErrorCode::OutputStorageAuthorityDoesNotMatch.into());
+        }
+        if self.output_storage.close_authority.is_some() {
+            return Err(ErrorCode::OutputStorageMustNotBeCloseable.into());
+        }
+        if self.output_storage.delegate.is_some() {
+            return Err(ErrorCode::OutputStorageMustNotBeDelegated.into());
+        }
+
+        *self.state = State {
+            admin_authority,
+            input_mint,
+            output_storage: self.output_storage.key(),
+            output_storage_authority_bump,
+        };
+
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+
+pub struct Swap<'info> {
+    #[account(has_one = input_mint, has_one = output_storage)]
+    pub state: Account<'info, State>,
+    #[account(mut)]
+    pub input: Account<'info, TokenAccount>,
+    pub input_authority: Signer<'info>,
+    #[account(mut)]
+    pub input_mint: Account<'info, Mint>,
+    #[account(mut)]
+    pub output_storage: Account<'info, TokenAccount>,
+    #[account(
+        seeds = [State::OUTPUT_STORAGE_AUTHORITY_SEED, &state.key().to_bytes()],
+        bump = state.output_storage_authority_bump
+    )]
+    pub output_storage_authority: UncheckedAccount<'info>,
+    #[account(mut)]
+    pub target: Account<'info, TokenAccount>,
+
+    pub token_program: Program<'info, Token>,
+}
+
+impl<'info> Swap<'info> {
+    pub fn process(&self, amount: u64) -> ProgramResult {
+        if self.input.mint != self.state.input_mint {
+            msg!("Expected input mint {}", self.state.input_mint);
+            return Err(ErrorCode::InvalidInputMint.into());
+        }
+
+        if self.input.owner != self.input_authority.key()
+            && !self.input.delegate.contains(&self.input_authority.key())
+        {
+            return Err(ErrorCode::InvalidInputAuthority.into());
+        }
+
+        if amount > self.output_storage.amount {
+            msg!("Requested swap {}/{}", amount, self.output_storage.amount);
+            return Err(ProgramError::InsufficientFunds);
+        }
+
+        token::burn(
+            CpiContext::new(
+                self.token_program.to_account_info(),
+                token::Burn {
+                    mint: self.input_mint.to_account_info(),
+                    to: self.input.to_account_info(),
+                    authority: self.input_authority.to_account_info(),
+                },
+            ),
+            amount,
+        )?;
+
+        token::transfer(
+            CpiContext::new_with_signer(
+                self.token_program.to_account_info(),
+                token::Transfer {
+                    from: self.output_storage.to_account_info(),
+                    to: self.target.to_account_info(),
+                    authority: self.output_storage_authority.to_account_info(),
+                },
+                &[&[
+                    State::OUTPUT_STORAGE_AUTHORITY_SEED,
+                    &self.state.key().to_bytes(),
+                    &[self.state.output_storage_authority_bump],
+                ]],
+            ),
+            amount,
+        )?;
+
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct Close<'info> {
+    #[account(mut, has_one = admin_authority, has_one = output_storage, close = rent_collector)]
+    pub state: Account<'info, State>,
+    pub admin_authority: Signer<'info>,
+    #[account(mut)]
+    pub output_storage: Account<'info, TokenAccount>,
+    #[account(
+        seeds = [State::OUTPUT_STORAGE_AUTHORITY_SEED, &state.key().to_bytes()],
+        bump = state.output_storage_authority_bump
+    )]
+    pub output_storage_authority: UncheckedAccount<'info>,
+    #[account(mut)]
+    pub token_target: Account<'info, TokenAccount>,
+    #[account(mut, owner = system_program::ID)]
+    pub rent_collector: UncheckedAccount<'info>,
+
+    pub token_program: Program<'info, Token>,
+}
+
+impl<'info> Close<'info> {
+    pub fn process(&mut self) -> ProgramResult {
+        if self.token_target.key() == self.state.output_storage.key() {
+            return Err(ErrorCode::InvalidCloseTokenTarget.into());
+        }
+        token::transfer(
+            CpiContext::new_with_signer(
+                self.token_program.to_account_info(),
+                token::Transfer {
+                    from: self.output_storage.to_account_info(),
+                    to: self.token_target.to_account_info(),
+                    authority: self.output_storage_authority.to_account_info(),
+                },
+                &[&[
+                    State::OUTPUT_STORAGE_AUTHORITY_SEED,
+                    &self.state.key().to_bytes(),
+                    &[self.state.output_storage_authority_bump],
+                ]],
+            ),
+            self.output_storage.amount,
+        )?;
+
+        token::close_account(CpiContext::new_with_signer(
+            self.token_program.to_account_info(),
+            token::CloseAccount {
+                account: self.output_storage.to_account_info(),
+                destination: self.rent_collector.to_account_info(),
+                authority: self.output_storage_authority.to_account_info(),
+            },
+            &[&[
+                State::OUTPUT_STORAGE_AUTHORITY_SEED,
+                &self.state.key().to_bytes(),
+                &[self.state.output_storage_authority_bump],
+            ]],
+        ))?;
+
+        **self
+            .rent_collector
+            .to_account_info()
+            .lamports
+            .as_ref()
+            .borrow_mut() += self.state.to_account_info().lamports();
+        **self.state.to_account_info().lamports.as_ref().borrow_mut() = 0;
+
+        Ok(())
+    }
+}
+
+#[error]
+pub enum ErrorCode {
+    #[msg("Treasury token authority does not match")]
+    OutputStorageAuthorityDoesNotMatch,
+    #[msg("Treasury token account must not be closeable")]
+    OutputStorageMustNotBeCloseable,
+    #[msg("Treasury token account must not be delegated")]
+    OutputStorageMustNotBeDelegated,
+    #[msg("Invalid input mint")]
+    InvalidInputMint,
+    #[msg("Invalid input authority")]
+    InvalidInputAuthority,
+    #[msg("Close token target must differ from storage")]
+    InvalidCloseTokenTarget,
+}
---
+++ tests/tokadapt.ts
@@ -0,0 +1,204 @@
+import * as anchor from '@project-serum/anchor';
+import * as token from '@solana/spl-token';
+import * as chai from 'chai';
+
+describe('tokadapt', () => {
+  // Configure the client to use the local cluster.
+  anchor.setProvider(anchor.Provider.env());
+
+  const adminAuthority = new anchor.web3.Keypair();
+  const inputMint = new anchor.web3.Keypair();
+  const outputMint = new anchor.web3.Keypair();
+  const outputStorage = new anchor.web3.Keypair();
+  const program = anchor.workspace.Tokadapt;
+  const state = new anchor.web3.Keypair();
+
+  before(async () => {
+
+    const inputAccount = await token.Token.getAssociatedTokenAddress(
+      token.ASSOCIATED_TOKEN_PROGRAM_ID, token.TOKEN_PROGRAM_ID, inputMint.publicKey, anchor.getProvider().wallet.publicKey);
+    const outputAccount = await token.Token.getAssociatedTokenAddress(
+      token.ASSOCIATED_TOKEN_PROGRAM_ID, token.TOKEN_PROGRAM_ID, outputMint.publicKey, anchor.getProvider().wallet.publicKey);
+
+    const [outputStorageAuthority, outputStorageAuthorityBump] =
+      await anchor.web3.PublicKey.findProgramAddress([
+        new TextEncoder().encode('storage'), state.publicKey.toBytes()],
+        program.programId);
+
+    const rentForMint = await token.Token.getMinBalanceRentForExemptMint(anchor.getProvider().connection);
+    // input
+    let transaction = new anchor.web3.Transaction({ feePayer: anchor.getProvider().wallet.publicKey });
+    transaction.add(
+      anchor.web3.SystemProgram.createAccount({
+        fromPubkey: anchor.getProvider().wallet.publicKey,
+        newAccountPubkey: inputMint.publicKey,
+        lamports: rentForMint,
+        space: token.MintLayout.span,
+        programId: token.TOKEN_PROGRAM_ID,
+      }),
+    );
+    transaction.add(token.Token.createInitMintInstruction(
+      token.TOKEN_PROGRAM_ID,
+      inputMint.publicKey,
+      9,
+      anchor.getProvider().wallet.publicKey, null));
+    transaction.add(token.Token.createAssociatedTokenAccountInstruction(
+      token.ASSOCIATED_TOKEN_PROGRAM_ID,
+      token.TOKEN_PROGRAM_ID,
+      inputMint.publicKey,
+      inputAccount,
+      anchor.getProvider().wallet.publicKey,
+      anchor.getProvider().wallet.publicKey
+    ));
+
+    console.log("Inputs", await anchor.getProvider().send(transaction, [inputMint]));
+
+    transaction = new anchor.web3.Transaction({ feePayer: anchor.getProvider().wallet.publicKey });
+
+    transaction.add(
+      anchor.web3.SystemProgram.createAccount({
+        fromPubkey: anchor.getProvider().wallet.publicKey,
+        newAccountPubkey: outputMint.publicKey,
+        lamports: rentForMint,
+        space: token.MintLayout.span,
+        programId: token.TOKEN_PROGRAM_ID,
+      }),
+    );
+    transaction.add(token.Token.createInitMintInstruction(
+      token.TOKEN_PROGRAM_ID,
+      outputMint.publicKey,
+      9,
+      anchor.getProvider().wallet.publicKey, null));
+
+    transaction.add(
+      anchor.web3.SystemProgram.createAccount({
+        fromPubkey: anchor.getProvider().wallet.publicKey,
+        newAccountPubkey: outputStorage.publicKey,
+        lamports: await token.Token.getMinBalanceRentForExemptAccount(anchor.getProvider().connection),
+        space: token.AccountLayout.span,
+        programId: token.TOKEN_PROGRAM_ID,
+      }),
+    );
+
+    transaction.add(token.Token.createInitAccountInstruction(
+      token.TOKEN_PROGRAM_ID,
+      outputMint.publicKey,
+      outputStorage.publicKey,
+      outputStorageAuthority));
+
+    transaction.add(token.Token.createAssociatedTokenAccountInstruction(
+      token.ASSOCIATED_TOKEN_PROGRAM_ID,
+      token.TOKEN_PROGRAM_ID,
+      outputMint.publicKey,
+      outputAccount,
+      anchor.getProvider().wallet.publicKey,
+      anchor.getProvider().wallet.publicKey
+    ));
+
+    console.log("Outputs", await anchor.getProvider().send(transaction, [outputMint, outputStorage]));
+
+    transaction = new anchor.web3.Transaction({ feePayer: anchor.getProvider().wallet.publicKey });
+    transaction.add(
+      anchor.web3.SystemProgram.createAccount({
+        fromPubkey: anchor.getProvider().wallet.publicKey,
+        newAccountPubkey: state.publicKey,
+        lamports: await anchor.getProvider().connection.getMinimumBalanceForRentExemption(1000),
+        space: 1000,
+        programId: program.programId,
+      })
+    )
+    console.log("State", await anchor.getProvider().send(transaction, [state]));
+    console.log("Initialize", await program.rpc.initialize(adminAuthority.publicKey, inputMint.publicKey, {
+      accounts: {
+        state: state.publicKey,
+        outputStorage: outputStorage.publicKey,
+      }
+    }));
+  });
+
+  it('Is swapping!', async () => {
+    const inputAccount = await token.Token.getAssociatedTokenAddress(
+      token.ASSOCIATED_TOKEN_PROGRAM_ID, token.TOKEN_PROGRAM_ID, inputMint.publicKey, anchor.getProvider().wallet.publicKey);
+    const outputAccount = await token.Token.getAssociatedTokenAddress(
+      token.ASSOCIATED_TOKEN_PROGRAM_ID, token.TOKEN_PROGRAM_ID, outputMint.publicKey, anchor.getProvider().wallet.publicKey);
+
+    const [outputStorageAuthority, outputStorageAuthorityBump] =
+      await anchor.web3.PublicKey.findProgramAddress([
+        new TextEncoder().encode('storage'), state.publicKey.toBytes()],
+        program.programId);
+
+    const transaction = new anchor.web3.Transaction({ feePayer: anchor.getProvider().wallet.publicKey });
+    transaction.add(
+      token.Token.createMintToInstruction(token.TOKEN_PROGRAM_ID, inputMint.publicKey, inputAccount, anchor.getProvider().wallet.publicKey, [], 10 * anchor.web3.LAMPORTS_PER_SOL)
+    );
+    transaction.add(
+      token.Token.createMintToInstruction(
+        token.TOKEN_PROGRAM_ID,
+        outputMint.publicKey,
+        outputStorage.publicKey,
+        anchor.getProvider().wallet.publicKey,
+        [], 100 * anchor.web3.LAMPORTS_PER_SOL
+      )
+    )
+
+    console.log("Prepare tokens", await anchor.getProvider().send(transaction, []));
+
+    const oldBalance = await anchor.getProvider().connection.getTokenAccountBalance(outputAccount);
+
+    console.log("Swap", await program.rpc.swap(new anchor.BN(2 * anchor.web3.LAMPORTS_PER_SOL), {
+      accounts: {
+        state: state.publicKey,
+        input: inputAccount,
+        inputAuthority: anchor.getProvider().wallet.publicKey,
+        inputMint: inputMint.publicKey,
+        outputStorage: outputStorage.publicKey,
+        outputStorageAuthority,
+        target: outputAccount,
+        tokenProgram: token.TOKEN_PROGRAM_ID,
+      }
+    }));
+
+    const balance = await anchor.getProvider().connection.getTokenAccountBalance(outputAccount)
+    chai.assert.isTrue(
+      new anchor.BN(balance.value.amount).eq(
+        new anchor.BN(oldBalance.value.amount)
+          .add(new anchor.BN(2 * anchor.web3.LAMPORTS_PER_SOL))));
+  });
+
+  it('Is closing!', async () => {
+    const outputAccount = await token.Token.getAssociatedTokenAddress(
+      token.ASSOCIATED_TOKEN_PROGRAM_ID, token.TOKEN_PROGRAM_ID, outputMint.publicKey, anchor.getProvider().wallet.publicKey);
+
+    const rentCollector = new anchor.web3.Keypair();
+    const [outputStorageAuthority, outputStorageAuthorityBump] =
+      await anchor.web3.PublicKey.findProgramAddress([
+        new TextEncoder().encode('storage'), state.publicKey.toBytes()],
+        program.programId);
+
+    const oldRent =
+      await anchor.getProvider().connection.getBalance(state.publicKey)
+      + await anchor.getProvider().connection.getBalance(outputStorage.publicKey);
+    const oldBalance = await anchor.getProvider().connection.getTokenAccountBalance(outputAccount);
+    const storageBalance = await anchor.getProvider().connection.getTokenAccountBalance(outputStorage.publicKey);
+    console.log("Close", await program.rpc.close({
+      accounts: {
+        state: state.publicKey,
+        adminAuthority: adminAuthority.publicKey,
+        outputStorage: outputStorage.publicKey,
+        outputStorageAuthority,
+        tokenTarget: outputAccount,
+        rentCollector: rentCollector.publicKey,
+        tokenProgram: token.TOKEN_PROGRAM_ID,
+      },
+      signers: [adminAuthority]
+    }));
+    const balance = await anchor.getProvider().connection.getTokenAccountBalance(outputAccount)
+    chai.assert.isTrue(
+      new anchor.BN(balance.value.amount).eq(
+        new anchor.BN(oldBalance.value.amount)
+          .add(new anchor.BN(storageBalance.value.amount))));
+
+    chai.assert.equal(await anchor.getProvider().connection.getBalance(rentCollector.publicKey), oldRent);
+
+  })
+});
